ab3e9a241ea0222478949a5142a073c22096b6f5
佐々木直 <sn.karintou@gmail.com>
2018-11-24 19:57:07 +0900

    add Dollar Class
    work but can't pass test

--- src/money/MoneyTest.java
+++ src/money/MoneyTest.java
@@ -1,13 +1,18 @@
 package money;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
 public class MoneyTest {
 	@Test
 	public void testMultiplication() {
+//		int型
+//		副作用？
 		Dollar five = new Dollar(5);
 		five.times(2);
-		assertEqauls(10, five.amount);
+//		amountに直アクセス
+		assertEquals(10, five.amount);
 	}
 
 }
