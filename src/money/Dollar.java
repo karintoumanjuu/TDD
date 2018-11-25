e71011e79c0fa641331e1da5ff5a888fe54470cb
佐々木直 <sn.karintou@gmail.com>
2018-11-25 15:58:44 +0900

    change test and make Dollar amount private
    test: use Dollar equal method

--- src/money/Dollar.java
+++ src/money/Dollar.java
@@ -1,16 +1,16 @@
 package money;
 
 class Dollar {
-	int amount;
-	
+	private int amount;
+
 	Dollar(int amount) {
 		this.amount = amount;
 	}
-	
+
 	Dollar times(int multipiler) {
 		return new Dollar(this.amount * multipiler);
 	}
-	
+
 	public boolean equals(Object object) {
 		Dollar dollar = (Dollar) object;
 		return this.amount == dollar.amount;
--- src/money/MoneyTest.java
+++ src/money/MoneyTest.java
@@ -7,16 +7,11 @@ import org.junit.jupiter.api.Test;
 public class MoneyTest {
 	@Test
 	public void testMultiplication() {
-//		副作用？->timesメソッドを呼び出すたびに内部で保持しているamountが変わるから
-//		ex) five.times(2) = 10, five.times(3) = 30(15じゃない)
-		
 		Dollar five = new Dollar(5);
-		Dollar product  = five.times(2);
-		assertEquals(10, product.amount);
-		product = five.times(3);
-		assertEquals(15, product.amount);
+		assertEquals(new Dollar(10), five.times(2));
+		assertEquals(new Dollar(15), five.times(3));
 	}
-	
+
 	@Test
 	public void testEquality() {
 		assertTrue(new Dollar(5).equals(new Dollar(5)));
