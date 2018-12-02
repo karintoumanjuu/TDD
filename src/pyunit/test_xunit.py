8ef97314f28873d89beea9111ed5dd532cfd379d
佐々木直 <sn.karintou@gmail.com>
2018-12-02 20:34:42 +0900

    catch exception caused by main method

--- src/pyunit/test_xunit.py
+++ src/pyunit/test_xunit.py
@@ -17,9 +17,17 @@ class TestCaseTest(xunit.TestCase):
         result = test.run()
         assert ("1 run, 1 failed" == result.summary())
 
+    def testFailedResultFormatting(self):
+        result = xunit.TestResult()
+        result.testStarted()
+        result.testFailed()
+        assert ("1 run, 1 failed" == result.summary())
+
 if __name__ == '__main__':
     TestCaseTest("testTemplateMethod").run()
     TestCaseTest("testResult").run()
-    # TestCaseTest("testFailedResult").run()
+    TestCaseTest("testFailedResult").run()
+    TestCaseTest("testFailedResultFormatting").run()
+
 
 
